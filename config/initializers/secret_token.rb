7369c2cea3c735c97998ea91f6417d628e860119
Cyril Zadvorniy <kir594793@mail.ru>
2012-10-21 21:40:04 +0400

    ASDF

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = 'b91ac8d743ad46ff79ab1ae50be5627860fcc369b05b9f56f2bf6e7240d60dae32cf5375864dea354ad9600f53b5668d8fd6dfcfbe31c9a91ca0622bf9dffe2c'
